1eeaed3eec05740078fe7d0011738d54fe7441db
Maxi Lucero Correa <mlucerocorrea@outlook.com>
2016-10-25 16:28:13 -0300

    constructor de Oferta.java

--- tp3_AlMejorPostor/src/model/Oferta.java
+++ tp3_AlMejorPostor/src/model/Oferta.java
@@ -1,17 +1,22 @@
 package model;
 
 import java.sql.Time;
+import java.util.List;
 
 public class Oferta 
 {
 	private Time _horaInicio;
 	private Time _horaFin;
 	private double _precio;
+	private String _nombre;
+	private List<String> _facilidades;
 	
-	public Oferta(Time horaInicio, Time horaFin, double precio)
+	public Oferta(Time horaInicio, Time horaFin, double precio, String nombre, List<String> facilidades)
 	{
 		_horaInicio = horaInicio;
 		_horaFin = horaFin;
 		_precio = precio;
+		_nombre = nombre;
+		_facilidades = facilidades;
 	}
 }
