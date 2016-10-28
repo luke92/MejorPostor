ba10753c67665143ac6817370165537a831630db
Maxi Lucero Correa <mlucerocorrea@outlook.com>
2016-10-28 14:44:03 -0300

    implementacion de Ofertas.java

---
+++ tp3_AlMejorPostor/src/controller/Ofertas.java
@@ -0,0 +1,66 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import model.Oferta;
+
+public class Ofertas implements Iterable<Oferta> {
+	private ArrayList<Oferta> _ofertas;
+	
+	public Ofertas()
+	{
+		_ofertas = new ArrayList<Oferta>();
+	}
+
+	public void agregar(Oferta o)
+	{
+		_ofertas.add(o);
+	}
+	
+	public void remover(Oferta o)
+	{
+		_ofertas.remove(o);
+	}
+	
+	public Oferta get(int index)
+	{
+		return _ofertas.get(index);
+	}
+	
+	public ArrayList<Oferta> getOfertas()
+	{
+		return _ofertas;
+	}
+	
+	public int size()
+	{
+		return _ofertas.size();
+	}
+	
+	public boolean contains(Oferta o)
+	{
+		for (Oferta of : _ofertas)
+			if (of.equals(o))
+				return true;
+		
+		return false;
+	}
+	
+	@Override
+	public Iterator<Oferta> iterator() {
+		return _ofertas.iterator();
+	}
+	
+	@Override
+	public String toString()
+	{
+		String ret = "";
+		for (Oferta o : _ofertas){
+			ret += o;
+			ret += "\n";
+		}
+		return ret;
+	}
+
+}
