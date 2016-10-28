9ba4913209e7c15e0ae726ea8129e8faa1c9c20f
Maxi Lucero Correa <mlucerocorrea@outlook.com>
2016-10-28 14:43:15 -0300

    implementacion de oferta.java

--- tp3_AlMejorPostor/src/model/Oferta.java
+++ tp3_AlMejorPostor/src/model/Oferta.java
@@ -1,22 +1,75 @@
 package model;
 
-import java.sql.Time;
 import java.util.List;
 
 public class Oferta 
 {
-	private Time _horaInicio;
-	private Time _horaFin;
+	
+	// TODO: le puse int para probar en el test
+	private int _horaInicio;
+	private int _horaFin;
 	private double _precio;
 	private String _nombre;
 	private List<String> _facilidades;
 	
-	public Oferta(Time horaInicio, Time horaFin, double precio, String nombre, List<String> facilidades)
+	public Oferta(int i, int j, double precio, String nombre, List<String> facilidades)
 	{
-		_horaInicio = horaInicio;
-		_horaFin = horaFin;
+		_horaInicio = i;
+		_horaFin = j;
 		_precio = precio;
 		_nombre = nombre;
 		_facilidades = facilidades;
 	}
+	
+	public int getHoraInicio(){
+		return _horaInicio;
+	}
+	
+	public int getHoraFin(){
+		return _horaFin;
+	}
+	
+	public double getPrecio(){
+		return _precio;
+	}
+	
+	public String getNombre(){
+		return _nombre;
+	}
+	
+	public List<String> facilidades(){
+		return _facilidades;
+	}
+	
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Oferta other = (Oferta) obj;
+		if (_horaInicio != other._horaInicio)
+			return false;
+		if (_horaFin != other._horaFin)
+			return false;
+		if (_precio != other._precio)
+			return false;
+		if (_nombre != other._nombre)
+			return false;
+		if (_facilidades != other._facilidades)
+			return false;
+		
+		return true;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "Nombre: " + _nombre + ". De " + _horaInicio + " a " + _horaFin + ". Precio: " + _precio + ". " + _facilidades ;
+	}
+	
 }
