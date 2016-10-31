46bdd937457f696ce002293b8cbff1d58602bf18
Maxi Lucero Correa <mlucerocorrea@outlook.com>
2016-10-31 15:40:24 -0300

    boton para cargarOfertas

--- tp3_AlMejorPostor/src/main/view/MainForm.java
+++ tp3_AlMejorPostor/src/main/view/MainForm.java
@@ -3,6 +3,11 @@ package main.view;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
 public class MainForm 
 {
 	public static void main(String[] args)
@@ -13,9 +18,22 @@ public class MainForm
 		juego.setSize(640, 480);
 		juego.setResizable(false);
 
-		juego.getContentPane().add(new OptionsForm("img/fondo.jpg"));
+		OptionsForm optionsForm = new OptionsForm("img/fondo.jpg");
+		juego.getContentPane().add(optionsForm);
+		
+		JButton btnCargarOfertas = new JButton("Cargar Ofertas");
+		btnCargarOfertas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cargarOfertas();
+			}
+		});
+		optionsForm.setLayout(null);
+		btnCargarOfertas.setBounds(238, 51, 121, 23);
+		optionsForm.add(btnCargarOfertas);
 
 		juego.setLocationRelativeTo(null);
 		juego.setVisible(true);
-	  }
+	}
+	
+	private static void cargarOfertas() {}
 }
