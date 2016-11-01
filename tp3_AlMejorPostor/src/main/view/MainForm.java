564a6a8c1e69c7cb372e1a3adf40d2a918b43109
Linkas <Linkas@Linkas-NB>
2016-10-31 21:47:32 -0300

    Reordenado el boton de Cargar Ofertas

--- tp3_AlMejorPostor/src/main/view/MainForm.java
+++ tp3_AlMejorPostor/src/main/view/MainForm.java
@@ -3,37 +3,18 @@ package main.view;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-
 public class MainForm 
 {
 	public static void main(String[] args)
 	{
-		JFrame juego = new JFrame();
-		juego.setTitle("Creizi Records Reservas");
-		juego.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		juego.setSize(640, 480);
-		juego.setResizable(false);
-
-		OptionsForm optionsForm = new OptionsForm("img/fondo.jpg");
-		juego.getContentPane().add(optionsForm);
-		
-		JButton btnCargarOfertas = new JButton("Cargar Ofertas");
-		btnCargarOfertas.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				cargarOfertas();
-			}
-		});
-		optionsForm.setLayout(null);
-		btnCargarOfertas.setBounds(238, 51, 121, 23);
-		optionsForm.add(btnCargarOfertas);
-
-		juego.setLocationRelativeTo(null);
-		juego.setVisible(true);
+		JFrame app = new JFrame();
+		app.setTitle("Creizi Records Reservas");
+		app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		app.setSize(640, 480);
+		app.setResizable(false);
+		app.getContentPane().add(new OptionsForm("img/fondo.jpg"));
+		app.setLocationRelativeTo(null);
+		app.setVisible(true);
 	}
 	
-	private static void cargarOfertas() {}
 }
--- tp3_AlMejorPostor/src/main/view/OptionsForm.java
+++ tp3_AlMejorPostor/src/main/view/OptionsForm.java
@@ -2,26 +2,48 @@ package main.view;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class OptionsForm extends JPanel {
 	private static final long serialVersionUID = 1L;
 	Image imagen;
-	String nombre;
+	String rutaImagen;
 
 	public OptionsForm(String nombre) 
 	{
-		this.nombre= nombre;
+		this.rutaImagen= nombre;
+		this.setLayout(null);
+		
+		JButton btnCargarOfertas = new JButton("Cargar Ofertas");
+		btnCargarOfertas.setBounds(238, 51, 121, 23);
+		btnCargarOfertas.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) {
+				cargarOfertas();
+			}
+
+		});
+		this.add(btnCargarOfertas);
+		
+		
 	}
 	
 	 public void paint(Graphics g)
 	 {
-		imagen = new ImageIcon(nombre).getImage();
+		imagen = new ImageIcon(rutaImagen).getImage();
 		g.drawImage(imagen, 0, 0, this.getWidth(), this.getHeight(), null);
 		setOpaque(false);
 		super.paint(g);
 	 }  
+	 
+	 private void cargarOfertas() 
+	 {
+			// TODO Auto-generated method stub		
+	}
 
 }
