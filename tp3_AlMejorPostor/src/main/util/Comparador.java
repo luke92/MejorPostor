00899b51b631315f28ff4c935500edbee8dc1aaf
Maxi Lucero Correa <mlucerocorrea@outlook.com>
2016-11-14 15:54:55 -0300

    arreglos menores

--- tp3_AlMejorPostor/src/main/util/Comparador.java
+++ tp3_AlMejorPostor/src/main/util/Comparador.java
@@ -27,39 +27,27 @@ public abstract class Comparador
 		return new Comparator<Oferta>() {
 			@Override
 			public int compare(Oferta uno, Oferta dos) {
-				if (mismoDia(uno, dos)) {
-					if (horariosSePisan(uno, dos))
-						return 1;
-					else
-						return 0;
-				} else
-					return uno.get_fecha().compareTo(dos.get_fecha());
+				if (horariosSePisan(uno, dos))
+					return 1;
+				else
+					return 0;
 			}
 		};
 	}
 
-	public static Comparator<Oferta> porBeneficio()	//si los horarios se pisan, retorna el precio de la oferta mayor
+	public static Comparator<Oferta> porBeneficio()
 	{
 		return new Comparator<Oferta>() {
 			@Override
 			public int compare(Oferta uno, Oferta dos) {
-				if (mismoDia(uno, dos)) {
-					if (horariosSePisan(uno, dos))
-						if (uno.get_precio() >= dos.get_precio())
-							return uno.get_precio();
-						else
-							return dos.get_precio();
-				}
+				if (horariosSePisan(uno, dos))
+					if (uno.get_precio() >= dos.get_precio())
+						return uno.get_precio();
+					else
+						return dos.get_precio();
 				return 0;
 			}
 		};
-	}
-	
-	private static boolean mismoDia(Oferta uno, Oferta dos)
-	{
-		if (uno.get_fecha().isEqual(dos.get_fecha()))
-			return true;
-		return false;
 	}
 	
 	private static boolean horariosSePisan(Oferta uno, Oferta dos)
@@ -72,6 +60,13 @@ public abstract class Comparador
 				return true;
 		}
 		return true;
+	}
+
+	private static boolean mismoDia(Oferta uno, Oferta dos)
+	{
+		if (uno.get_fecha().isEqual(dos.get_fecha()))
+			return true;
+		return false;
 	}
 
 }
